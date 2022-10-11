5edc5d5f2cb652a014065ab7b6e44c6c9e97e87b
Eugenio Tampieri <eugenio@eutampieri.eu>
2022-10-11 11:33:51 +0200

    Display chords

--- res/song.js
+++ res/song.js
@@ -7,6 +7,18 @@ function get_soundcloud_rec(url) {
             return element;
         });
 }
+function print_chord(chord, prefix) {
+    let chord_el = document.createElement("span");
+    let chord_text = document.createElement("span");
+    chord_text.innerText = chord
+    let chord_prefix = document.createElement("span");
+    chord_prefix.innerText = prefix
+    chord_prefix.style.visibility = "hidden";
+    chord_prefix.ariaHidden = true;
+    chord_el.appendChild(chord_prefix);
+    chord_el.appendChild(chord_text);
+    return chord_el;
+}
 async function load() {
     let conf = await fetch("conf.json").then(x => x.json());
     create_navbar(conf, document.getElementsByTagName("nav")[0]);
@@ -14,14 +26,39 @@ async function load() {
     let song = await fetch(song_file).then(x => x.json());
     document.getElementsByTagName("title")[0].innerText = `${conf.site_name} - ${song.title}`;
     document.getElementsByTagName("h1")[0].innerText = song.title;
+    let chord_map = [];
+    for (const l of song.lyrics) {
+        chord_map.push([]);
+        for (const _ of l) {
+            chord_map[chord_map.length - 1].push([]);
+        }
+    }
+    for (const chord of song.chords) {
+        chord_map[chord.stanza][chord.row].push(print_chord(chord.chord, song.lyrics[chord.stanza][chord.row].substring(0, chord.position)));
+    }
     let lyrics = document.getElementById("lyrics");
     song.lyrics.forEach((stanza, i) => {
         let s = document.createElement("p");
-        s.innerHTML = stanza.join("<br>");
+        lyrics.appendChild(s);
+        stanza.forEach((row, j) => {
+            if (chord_map[i][j].length > 0) {
+                let chords = document.createElement("div");
+                s.appendChild(chords);
+                let current_offset = 0;
+                for (let c of chord_map[i][j]) {
+                    c.style.marginLeft = `${current_offset * -1}px`;
+                    chords.appendChild(c);
+                    current_offset = c.offsetWidth;
+                }
+                chords.classList.add("chord");
+            }
+            let r = document.createElement("div");
+            r.innerText = row;
+            s.appendChild(r);
+        });
         if (song.refrain[i]) {
             s.classList.add("refrain");
         }
-        lyrics.appendChild(s);
     });
     let tag_list = document.getElementById("tags");
     for (const tag of song.tags) {
