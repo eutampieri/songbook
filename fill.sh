acce0bf2f05b4b88fa814c30f66327ff7d139694
Eugenio Tampieri <eugenio@eutampieri.eu>
2022-10-22 10:59:27 +0200

    Conditionally build rust; debug

--- fill.sh
+++ fill.sh
@@ -3,8 +3,13 @@ CURRENT_COMMIT=`git rev-parse --short HEAD`#$CI_COMMIT_SHORT_SHA
 AUTHORS=`git shortlog -sn | cut -d$'\t' -f 2 | grep -v eutampieri | perl -p -e 'chomp if eof' | tr '\n' ',' | sed 's/,/, /'`
 COPYRIGHT_DATES=`git shortlog --format=format:%cI | grep - | sort | cut -d '-' -f1 | uniq | tr -d ' ' | perl copyright.pl`
 
+echo "Current commit: $CURRENT_COMMIT"
+echo "Authors: $AUTHORS"
+echo "First and last commit years: $COPYRIGHT_DATES"
+
 for f in *.html
-    do sed -i "s/{{version}}/$CURRENT_COMMIT/g" $f
+    do echo "Processing $f..."
+    sed -i "s/{{version}}/$CURRENT_COMMIT/g" $f
     sed -i "s/{{authors}}/$AUTHORS/g" $f
     sed -i "s/{{dates}}/$COPYRIGHT_DATES/g" $f
 done
