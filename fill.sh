27484bf36faaa5cf82e99a9d4c6e526470da1e70
Eugenio Tampieri <eugenio@eutampieri.eu>
2022-10-22 10:46:46 +0200

    Fix CI

--- fill.sh
+++ fill.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CURRENT_COMMIT=`git rev-parse --short HEAD`#$CI_COMMIT_SHORT_SHA
 AUTHORS=`git shortlog -sn | cut -d$'\t' -f 2 | grep -v eutampieri | perl -p -e 'chomp if eof' | tr '\n' ',' | sed 's/,/, /'`
-COPYRIGHT_DATES = `git shortlog --format=format:%cI | grep - | sort | cut -d '-' -f1 | uniq | tr -d ' ' | perl copyright.pl`
+COPYRIGHT_DATES=`git shortlog --format=format:%cI | grep - | sort | cut -d '-' -f1 | uniq | tr -d ' ' | perl copyright.pl`
 
 for f in *.html
     do sed -i "s/{{version}}/$CURRENT_COMMIT/g" $f
