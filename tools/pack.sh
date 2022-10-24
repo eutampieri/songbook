e913958846f6ab489972c29cbc8f5113fa432528
Eugenio Tampieri <eugenio@eutampieri.eu>
2022-10-24 11:14:42 +0200

    Improve directory structure

--- tools/pack.sh
+++ tools/pack.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CURRENT_COMMIT=`git rev-parse --short HEAD`#$CI_COMMIT_SHORT_SHA
 AUTHORS=`git log | git shortlog -sn | cut -d$'\t' -f 2 | grep -v eutampieri | perl -p -e 'chomp if eof' | tr '\n' ',' | sed 's/,/, /'`
-COPYRIGHT_DATES=`git log --format=format:%cI | grep - | sort | cut -d '-' -f1 | uniq | tr -d ' ' | perl copyright.pl`
+COPYRIGHT_DATES=`git log --format=format:%cI | grep - | sort | cut -d '-' -f1 | uniq | tr -d ' ' | perl tools/copyright.pl`
 
 perl -v
 
@@ -15,3 +15,5 @@ for f in *.html
     sed -i "s/{{authors}}/$AUTHORS/g" $f
     sed -i "s/{{dates}}/$COPYRIGHT_DATES/g" $f
 done
+
+mv tools/make_index.py .
