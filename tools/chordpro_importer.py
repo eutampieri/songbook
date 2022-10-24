53eaabe2e7b89c1fd82dca1748fd8c65dadafff3
Eugenio Tampieri <eugenio@eutampieri.eu>
2022-10-24 18:12:04 +0200

    Working a bit better

--- tools/chordpro_importer.py
+++ tools/chordpro_importer.py
@@ -13,7 +13,7 @@ def parse_line(l):
             chord = ""
         elif c == ']':
             in_chord = False
-            chords.append({"position": len(chord), "chord": chord})
+            chords.append({"position": len(line), "chord": chord})
         elif in_chord:
             chord += c
         elif not in_chord:
@@ -29,9 +29,9 @@ def parse_directive(d):
         return parsed[0], parsed[1]
 
 
-def fix_chord(c, stanza, verse):
+def fix_chord(c, stanza, row):
     c["stanza"] = stanza
-    c["verse"] = verse
+    c["row"] = row
     return c
 
 
@@ -39,19 +39,34 @@ for filename in glob("songs/*.crd"):
     current_stanza = 0
     current_verse = 0
     song = {"title": "", "tags": [],
-            "authors": [], "lyrics": [[]], "refrain": [], "chords": []}
+            "authors": [], "lyrics": [[]], "refrain": [False], "chords": []}
     with open(filename) as f:
         chordpro = f.readlines()
     for line in chordpro:
+        line = line.strip()
+        if len(line) == 0:
+            continue
         if line[0] == '#':
             continue
         elif line[0] == '{':
             dir, val = parse_directive(line)
             if dir == "title":
                 song["title"] = val
-        line = line.strip()
+            elif dir == "soc":
+                song["lyrics"].append([])
+                song["refrain"].append(True)
+                current_stanza += 1
+                current_verse = 0
+            elif dir == "eoc":
+                song["lyrics"].append([])
+                song["refrain"].append(False)
+                current_stanza += 1
+                current_verse = 0
+            else:
+                print(dir, val)
+            continue
         line, chords = parse_line(line)
-        song["lyrics"][0].append(line)
+        song["lyrics"][len(song["lyrics"]) - 1].append(line)
         for c in chords:
             song["chords"].append(fix_chord(c, current_stanza, current_verse))
         current_verse += 1
